b30c323678d8d2f387b943c2e5520b8a74346008
hangminhson <95537221+hangminhson@users.noreply.github.com>
2022-04-04 21:35:21 +0700

    Add files via upload

---
+++ Dichso/src/s61134291/Dichso_Client.java
@@ -0,0 +1,69 @@
+package s61134291;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Dichso_Client{
+    public static void main(String[] args) throws IOException
+
+    {
+        Socket mySocket = null;
+
+        DataOutputStream dos = null;
+
+        DataInputStream dis=null;
+
+       
+
+        try {
+
+            mySocket = new Socket("localhost", 8888);
+
+            dos = new DataOutputStream(mySocket.getOutputStream());
+
+            dis = new DataInputStream(mySocket.getInputStream());
+
+            Scanner input = new Scanner(System.in);
+
+            String s = null;
+
+            while(true)
+
+            {
+
+                System.out.print("\nNhập số: ");
+
+                s =input.nextLine();
+
+                dos.writeUTF(s);
+
+               
+
+                String str=dis.readUTF();
+
+                System.out.print("Kết quả từ Server : "+str);
+
+            }
+
+        }
+
+        catch(Exception e) {
+
+            System.out.print("Dừng");
+
+            dis.close();
+
+            dos.close();
+
+            mySocket.close();
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
---
+++ Dichso/src/s61134291/Dichso_Server.java
@@ -0,0 +1,83 @@
+package s61134291;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Dichso_Server {
+	public static void main(String[] args) throws IOException{
+
+	    DataOutputStream dos = null;
+
+	    DataInputStream dis=null;
+
+	    try {
+
+	    	ServerSocket serverSocket = new ServerSocket(8888);
+
+	    	System.out.print("Tôi lắng nghe trong cổng 8888 ...");
+
+	        Socket clientSocket = null;
+
+	        clientSocket = serverSocket.accept();
+
+	        dos=new DataOutputStream(clientSocket.getOutputStream());
+
+	        dis=new DataInputStream(clientSocket.getInputStream());
+
+	        String inline="";
+
+	        while(true)
+
+	        {
+
+	            inline = dis.readUTF();
+
+	            char ch[]=inline.toCharArray();
+
+	            if(Character.isDigit(ch[0]))
+
+	                {
+
+	                int i=Integer.parseInt(inline);
+
+	                switch(i)
+
+	                {
+
+	                    case 0:inline="Zero";break;
+	                    case 1:inline="One";break;
+	                    case 2:inline="Two";break;
+	                    case 3:inline="Three";break;
+	                    case 4:inline="for";break;
+	                    case 5:inline="five";break;
+	                    case 6:inline="six";break;
+	                    case 7:inline="seven";break;
+	                    case 8:inline="eight";break;
+	                    case 9:inline="nine	";break;
+
+	                }
+
+	                dos.writeUTF(inline);
+
+	            }
+
+	            else
+
+	                dos.writeUTF("Thoát");
+
+	        }
+
+	    }
+
+	    catch(Exception e) {
+
+	        System.out.print(e.getMessage());
+
+	    }
+
+	}
+
+	}
